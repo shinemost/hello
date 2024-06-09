95905ab61c95c5051e5a1f375e457962ce054f10
shinemost <supertain147@163.com>
2024-06-09 12:18:14 +0800

    结构体和所有权

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,56 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2024-06-05 22:36:45
- * @LastEditTime: 2024-06-09 11:12:50
+ * @LastEditTime: 2024-06-09 12:17:34
  * @FilePath: /hello/src/main.rs
  */
 fn main() {
-    let drink = Drink {
-        flavor: Flavor::Sweet,
-        price: 6.0,
-    };
-    println!("{}", drink.price);
-    // print_drink(drink);//所有权转移
-    drink.buy();
-    let drink = Drink::new(12.0);
-    drink.buy();
+    let c1 = Counter::new(1);
+    println!("{}", c1.number);
+
+    c1.get_number();
+    println!("{}", c1.number);
+    println!("{}", c1.number);
+
+    let mut c1 = Counter::new(2);
+    c1.add(32);
+    println!("mut c1 {}", c1.number);
+    println!("mut c1 {}", c1.number);
+
+    // Counter::giveup(c1);
+    c1.giveup();
+    // println!("mut c1 {}", c1.number);
+
+    let c1 = Counter::new(3);
+    let c2 = Counter::new(4);
+    let c3 = Counter::combine(c1, c2);
+    println!("{}", c3.number);
+    // println!("{}", c1.number);
+    // println!("{}", c2.number);
 }
 
-enum Flavor {
-    Sweet,
-    Fruity,
-    Splicy,
+struct Counter {
+    number: i32,
 }
 
-struct Drink {
-    flavor: Flavor,
-    price: f64,
-}
+impl Counter {
+    fn new(number: i32) -> Self {
+        Counter { number }
+    }
 
-impl Drink {
-    // 关联变量
-    const MAX_PRICE: f64 = 10.0;
-    //方法
-    fn buy(&self) {
-        if self.price > Drink::MAX_PRICE {
-            println!("I am poor");
-            return;
+    fn get_number(&self) -> i32 {
+        self.number
+    }
+    fn add(&mut self, increment: i32) {
+        self.number += increment
+    }
+
+    fn giveup(self) {
+        println!("free {}", self.number)
+    }
+    fn combine(c1: Self, c2: Self) -> Self {
+        Counter {
+            number: c1.number + c2.number,
         }
-        println!("buy it");
     }
-
-    //关联函数
-    fn new(price: f64) -> Self {
-        Drink {
-            flavor: Flavor::Fruity,
-            price,
-        }
-    }
-}
-// 普通函数
-fn print_drink(drink: Drink) {
-    match drink.flavor {
-        Flavor::Sweet => println!("sweet"),
-        Flavor::Splicy => println!("splicy"),
-        Flavor::Fruity => println!("fruity"),
-    }
-    println!("{}", drink.price);
 }
