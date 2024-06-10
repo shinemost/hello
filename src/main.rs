21deb59ffcb18a4cbbeb12cf7169b7638cfc8c20
shinemost <supertain147@163.com>
2024-06-10 22:44:58 +0800

    rust里的几种循环和迭代的使用

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,66 @@
-/*
- * @LastEditors: shinemost supertain147@163.com
- * @Date: 2024-06-05 22:36:45
- * @LastEditTime: 2024-06-10 12:28:32
- * @FilePath: /hello/src/main.rs
- */
 fn main() {
-    let age = 90;
+    // loop {
+    //     println!("ctrl c");
+    //     std::thread::sleep(time::Duration::from_secs(1));
+    // }
 
-    // if的表达能力很弱，只适合简单的场景，if嵌套会影响可读性
-    if age > 50 {
-        println!("you are old");
-    } else {
-        println!("you are young");
+    let mut i = 0;
+    while i < 10 {
+        println!("{i}");
+        i += 1;
+    }
+    let v = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for elem in v {
+        println!("{elem}");
     }
 
-    let num = 90;
-    if num > 90 {
-        println!(" very good");
-    } else if num > 60 {
-        println!("good");
-    } else {
-        println!("bad");
+    for i in 0..10 {
+        println!("{i}");
     }
 
-    let msg = if num > 90 {
-        "excellent".to_owned()
-    } else {
-        "good".to_owned()
-    };
-    println!("you are {msg}");
-
-    // match 表达性很强，适用于复杂业务场景，可以与if搭配使用
-    match num {
-        80 => println!("good"),
-        90 => println!("excellent"),
-        _ => println!("Some else"),
+    for i in 0..=10 {
+        println!("{i}");
     }
 
-    match num {
-        60..=100 => println!("good"),
-        20..=59 => println!("bad"),
-        _ => println!("some else"),
+    for i in 0..12 {
+        if i == 10 {
+            break;
+        }
+        println!("{i}");
     }
 
-    match num {
-        25 | 50 | 75 => println!("25 or 50 or 75"),
-        80 | 100 => println!("80 or 100"),
-        _ => println!("some else"),
+    for i in 0..12 {
+        if i == 10 {
+            continue;
+        }
+        println!("{i}");
     }
 
-    match num {
-        x if x > 90 => println!("excellent"),
-        x if x > 60 => println!("good"),
-        _ => println!("some else"),
+    // loop {
+    //     println!("outer");
+    //     loop {
+    //         println!("inner");
+    //         break;
+    //     }
+    // }
+    's: loop {
+        println!("outer");
+        loop {
+            println!("inner");
+            break 's;
+        }
     }
-    // if 和match都可以作为表达式返回
-    let msg = match num {
-        x if x > 90 => "excellent".to_owned(),
-        x if x > 60 => "good".to_owned(),
-        _ => "some else".to_owned(),
-    };
-    println!("you are {msg}");
+    // 循环的使用
+    let v = [1, 2, 3, 4, 5];
+    let mut s = Vec::new();
+    for &n in v.iter() {
+        let m = &n * &n;
+        s.push(m);
+    }
+    println!("{:?}", s);
+
+    // 迭代的使用
+    let mut v = [1, 2, 3, 4, 5].to_vec();
+    let s: Vec<i32> = v.iter().map(|&s| s * s).collect();
+    println!("{:?}", s);
 }
