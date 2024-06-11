a2786206a7a3e46a4b9543813ee50f66dc002872
shinemost <supertain147@163.com>
2024-06-11 22:38:07 +0800

    高阶函数，使用函数作为函数的入参和返回值，常用于集合的处理上，类似于java的stream流式处理

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
-// 返回copy
-fn copy_back() -> i32 {
-    4
+fn func_twice(f: fn(i32) -> i32, x: i32) -> i32 {
+    f(f(x))
 }
 
-// 不返回copy,直接移交所有权
-fn copy_no_back() -> String {
-    "hello".to_string()
+fn mute(x: i32) -> i32 {
+    x * x
 }
 
-// 返回引用
-fn borrow_back(x: i32) -> &'static str {
-    if x == 0 { "🤣😁" } else { "😭😎" }
+fn add(x: i32) -> i32 {
+    x + x
 }
-
 
 fn main() {
-    let x = copy_back();
-    println!("{x}");
-    let y = copy_no_back();
-    println!("{y}");
+    let x = 3;
+    let r = func_twice(mute, x);
+    println!("{r}");
 
-    let s = borrow_back(x);
-    println!("{s}");
+    let x = 10;
+    let r = func_twice(add, x);
+    println!("{r}");
+
+    let v: Vec<i32> = [1, 2, 3, 4, 5, 6, 7].to_vec();
+    let map: Vec<i32> = v.iter().map(|&x| x * 2).collect();
+    println!("{:?}", map);
+
+    let v: Vec<_> = [1, 2, 3, 4, 5, 6, 7].to_vec();
+    let filter = v.iter().filter(|&x| x % 2 == 0).collect::<Vec<_>>();
+    println!("{:?}", filter);
+
+    let v: Vec<_> = [1, 2, 3, 4, 5, 6, 7].to_vec();
+    let sum = v.iter().fold(0, |acc, &x| acc + x);
+    println!("{sum}");
 }
 
 
