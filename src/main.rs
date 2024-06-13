946e5414a6c00d9e2f4e3598daa21f76ac26d2fc
shinemost <supertain147@163.com>
2024-06-13 21:34:01 +0800

    unwrap与?的用法

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,54 @@
-fn dived(x: i32, y: i32) -> Result<f64, String> {
-    if y == 0 {
-        Err("can not be zero".to_string())
-    } else {
-        let x = x as f64;
-        let y = y as f64;
-        Ok(x / y)
+use std::num::ParseIntError;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result_ok: Result<i32, &str> = Ok(32);
+    let value = result_ok.unwrap();
+    println!("{value}");
+
+    // unwrap遇到error 会直接panic
+    // let result_error: Result<i32, &str> = Err("FFF");
+    // let value = result_error.unwrap();
+    // println!("{value}");
+
+    let result_ok: Result<i32, &str> = Ok(32);
+    let value = result_ok?;
+    println!("{value}");
+
+
+    let v = vec![1, 3, 5];
+    match find_first_even(v) {
+        None => println!("not found even"),
+        Some(i) => println!("{i}"),
     }
-}
 
-fn find_elements(v: &[i32], target: i32) -> Option<usize> {
-    for (index, element) in v.iter().enumerate() {
-        if *element == target {
-            return Some(index);
-        }
+    let mm = "234";
+    match parse_number(mm) {
+        Ok(i) => println!("{i}"),
+        Err(err) => println!("error,{}", err)
     }
-    None
+
+    let mm = "d";
+    match parse_number(mm) {
+        Ok(i) => println!("{i}"),
+        Err(err) => println!("error,{}", err)
+    }
+
+
+    Ok(())
 }
 
-
-fn main() {
-    match dived(1, 2) {
-        Ok(number) => println!("{number}"),
-        Err(msg) => println!("{msg}"),
-    };
-    match dived(1, 0) {
-        Ok(number) => println!("{number}"),
-        Err(msg) => println!("{msg}"),
-    };
-
-    let v = vec![1, 2, 3, 4, 5];
-    match find_elements(&v, 4) {
-        None => println!("can not found"),
-        Some(index) => println!("found in {index}"),
-    };
-
-    match find_elements(&v, 7) {
-        None => println!("can not found"),
-        Some(index) => println!("found in {index}"),
-    };
-
-    // v[43];//被动引发panci
-
-    panic!("主动引发panic");
-
-    //执行查看堆栈信息
-//      RUST_BACKTRACE=1 cargo run
-//      RUST_BACKTRACE=full cargo run
+// ？如果遇到None，提前返回
+fn find_first_even(v: Vec<i32>) -> Option<i32> {
+    let v = v.iter().find(|&x| x % 2 == 0)?;
+    Some(*v)
 }
 
+// ? 如果遇到error,提前返回
+fn parse_number(s: &str) -> Result<i32, ParseIntError> {
+    let m = s.parse()?;
+    println!("{s}");
+    Ok(m)
+}
 
 
 
