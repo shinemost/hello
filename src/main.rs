701b1acf5eac0031061ed3f6b96daca75f502973
shinemost <supertain147@163.com>
2024-06-13 22:59:39 +0800

    自定义Error

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,44 @@
-use std::num::ParseIntError;
+use std::fmt::Formatter;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let result_ok: Result<i32, &str> = Ok(32);
-    let value = result_ok.unwrap();
-    println!("{value}");
-
-    // unwrap遇到error 会直接panic
-    // let result_error: Result<i32, &str> = Err("FFF");
-    // let value = result_error.unwrap();
-    // println!("{value}");
-
-    let result_ok: Result<i32, &str> = Ok(32);
-    let value = result_ok?;
-    println!("{value}");
-
-
-    let v = vec![1, 3, 5];
-    match find_first_even(v) {
-        None => println!("not found even"),
-        Some(i) => println!("{i}"),
+fn main() -> Result<(), Box<MyError>> {
+    match func() {
+        Err(error) => println!("{:?}", error),
+        Ok(()) => (),
     }
-
-    let mm = "234";
-    match parse_number(mm) {
-        Ok(i) => println!("{i}"),
-        Err(err) => println!("error,{}", err)
-    }
-
-    let mm = "d";
-    match parse_number(mm) {
-        Ok(i) => println!("{i}"),
-        Err(err) => println!("error,{}", err)
-    }
-
-
+    let s = func()?;
+    println!("{:?}", s);
+    println!("oo");
     Ok(())
 }
 
-// ？如果遇到None，提前返回
-fn find_first_even(v: Vec<i32>) -> Option<i32> {
-    let v = v.iter().find(|&x| x % 2 == 0)?;
-    Some(*v)
+#[derive(Debug)]
+struct MyError {
+    detail: String,
 }
 
-// ? 如果遇到error,提前返回
-fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    let m = s.parse()?;
-    println!("{s}");
-    Ok(m)
+
+impl std::error::Error for MyError {
+    fn description(&self) -> &str {
+        &self.detail
+    }
 }
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MyError {}", self.detail)
+    }
+}
+
+fn func() -> Result<(), MyError> {
+    Err(MyError {
+        detail: "CustomError".to_owned(),
+    })
+    // Ok(())
+}
+
+
+
+
 
 
 
