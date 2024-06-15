f8a2d59d72086518427d8c91aabf8d72a332f5da
shinemost <supertain147@163.com>
2024-06-15 21:48:32 +0800

    声明周期与结构体

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,63 @@
-fn main() {
-    let s1 = "hello world";
-    let s2 = "hello";
+struct MyString<'a> {
+    text: &'a str,//强烈不建议这样使用，结构体标注生命周期会很麻烦，实现方法，关联函数都要标注，看得很烦，而且容易出错，直接使用String
+}
 
-    println!("no need {}", no_need(s1));
-    println!("longest {}", longest2(s1, s2));
-
-
-    let result: &str;
-    {
-        let r2 = "world";
-        result = longest3(s1, r2);
-        println!("longest str: {}", result);
+impl<'a> MyString<'a> {
+    fn get_length(&self) -> usize {
+        self.text.len()
+    }
+    fn modify_text(&mut self) {
+        self.text = "world";
     }
 }
 
-// 只有一个引用入参和返回值，不需要标注生命周期
-fn no_need(s1: &str) -> &str {
-    s1
+struct StringHolder {
+    detail: String,
 }
 
-// 两个入参，必须要标注生命周期，此处是将所有的入参和返回值标注为同一个生命周期
-fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
-    s1
+impl StringHolder {
+    fn get_length(&self) -> usize {
+        self.detail.len()
+    }
+
+    fn get_reference(&self) -> &String {
+        &self.detail
+    }
+
+    fn get_ref<'a>(&'a self) -> &'a String {
+        &self.detail
+    }
+
+    fn modify_text(&mut self) -> &mut String {
+        &mut self.detail
+    }
 }
 
-// 使用了static生命周期，存在于整个程序运行期间，不建议用
-fn longest2<'a>(s1: &'static str, s2: &'a str) -> &'static str {
-    s1
+
+fn main() {
+    let mut s = MyString {
+        text: "hello",
+    };
+    println!("{}", s.get_length());
+    s.modify_text();
+    println!("{}", s.text);
+
+    let n = StringHolder {
+        detail: String::from("like you"),
+    };
+    println!("n: {}", n.get_length());
+    println!("n: {}", n.get_reference());
+    println!("n: {}", n.get_ref());
+
+    let mut m = StringHolder {
+        detail: String::from("like you"),
+    };
+    let mm = m.modify_text();
+    *mm = "not like you".to_owned();
+    println!("m: {}", m.get_length());
+    println!("m: {}", m.get_reference());
+    println!("m: {}", m.get_ref());
 }
 
-// 显示声明了不同的生命周期，同时标注出生命周期之间的包含关系，比较灵活，但是不够简便。
-fn longest3<'a, 'b, 'out>(s1: &'a str, s2: &'b str) -> &'out str
-// 显示声明'a，'b生命周期都包含'out
-    where 'a: 'out, 'b: 'out {
-    if s1.len() > s2.len() {
-        s1
-    } else { s2 }
-}
 
 
