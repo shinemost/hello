c26e3ecc1a3f7c12d3d0f9e6e9d775ec502d46dd
shinemost <supertain147@163.com>
2024-08-15 09:10:27 +0000

    为自定义类型实现 + 操作

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,40 @@
-fn swap<T>(x: T, y: T) -> (T, T) {
-    (y, x)
-}
+use std::ops::Add;
 
-struct Point<T> {
+// 为Point结构体派生Debug特征，用于格式化输出
+#[derive(Debug)]
+struct Point<T: Add<T, Output = T>> {
+    //限制类型T必须实现了Add特征，否则无法进行+操作。
     x: T,
     y: T,
 }
 
-impl<T> Point<T> {
-    fn new(x: T, y: T) -> Self {
-        Point { x, y }
-    }
-    // 返回引用，不用返回示例，针对基本类型和字符串等分两种情况，默认实现COPY和未显示copy的，
-    // 即存在所有权的区别。
-    fn get_reference(&self) -> (&T, &T) {
-        (&self.x, &self.y)
+impl<T: Add<T, Output = T>> Add for Point<T> {
+    type Output = Point<T>;
+
+    fn add(self, p: Point<T>) -> Point<T> {
+        Point {
+            x: self.x + p.x,
+            y: self.y + p.y,
+        }
     }
 }
 
+fn add<T: Add<T, Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
 fn main() {
-    let x = 3;
-    let y = 4;
-    let r1 = swap(x, y);
-    println!("{:?}", r1);
+    let p1 = Point {
+        x: 1.1f32,
+        y: 1.1f32,
+    };
+    let p2 = Point {
+        x: 2.1f32,
+        y: 2.1f32,
+    };
+    println!("{:?}", add(p1, p2));
 
-    let x = 3.01;
-    let y = 4.01;
-    let r1 = swap(x, y);
-    println!("{:?}", r1);
-
-    let ruslt = swap("hello", "world");
-    println!("{:?}", ruslt);
-
-    let i32_point = Point::new(1, 2);
-    let (x, y) = i32_point.get_reference();
-    println!("i32 point x: {},y: {}", x, y);
-
-    let f64_point = Point::new(1.2, 2.1);
-    let (x, y) = f64_point.get_reference();
-    println!(" f64 point x: {},y: {}", x, y);
-
-    let string_point = Point::new("hello".to_owned(), "world".to_string());
-    let (x, y) = string_point.get_reference();
-    println!("string point x: {},y: {}", x, y);
+    let p3 = Point { x: 1i32, y: 1i32 };
+    let p4 = Point { x: 2i32, y: 2i32 };
+    println!("{:?}", add(p3, p4));
 }
