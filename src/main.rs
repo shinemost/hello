a53529180642fd1bc6b9b520eaf9a590d42bf2fc
shinemost <supertain147@163.com>
2024-08-15 09:17:24 +0000

    特征，为不同结构体实现同一个方法，类似于接口

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
-use std::ops::Add;
-
-// 为Point结构体派生Debug特征，用于格式化输出
-#[derive(Debug)]
-struct Point<T: Add<T, Output = T>> {
-    //限制类型T必须实现了Add特征，否则无法进行+操作。
-    x: T,
-    y: T,
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+pub struct Post {
+    pub title: String,   // 标题
+    pub author: String,  // 作者
+    pub content: String, // 内容
 }
 
-impl<T: Add<T, Output = T>> Add for Point<T> {
-    type Output = Point<T>;
-
-    fn add(self, p: Point<T>) -> Point<T> {
-        Point {
-            x: self.x + p.x,
-            y: self.y + p.y,
-        }
+impl Summary for Post {
+    fn summarize(&self) -> String {
+        format!("文章{}, 作者是{}", self.title, self.author)
     }
 }
 
-fn add<T: Add<T, Output = T>>(a: T, b: T) -> T {
-    a + b
+pub struct Weibo {
+    pub username: String,
+    pub content: String,
+}
+
+impl Summary for Weibo {
+    fn summarize(&self) -> String {
+        format!("{}发表了微博{}", self.username, self.content)
+    }
 }
 
 fn main() {
-    let p1 = Point {
-        x: 1.1f32,
-        y: 1.1f32,
+    let post = Post {
+        title: "Rust语言简介".to_string(),
+        author: "Sunface".to_string(),
+        content: "Rust棒极了!".to_string(),
     };
-    let p2 = Point {
-        x: 2.1f32,
-        y: 2.1f32,
+    let weibo = Weibo {
+        username: "sunface".to_string(),
+        content: "好像微博没Tweet好用".to_string(),
     };
-    println!("{:?}", add(p1, p2));
 
-    let p3 = Point { x: 1i32, y: 1i32 };
-    let p4 = Point { x: 2i32, y: 2i32 };
-    println!("{:?}", add(p3, p4));
+    println!("{}", post.summarize());
+    println!("{}", weibo.summarize());
 }
