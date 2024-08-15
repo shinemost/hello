6787218e06840dc6a7bfe048852a28ccaa74caf5
shinemost <supertain147@163.com>
2024-08-15 09:21:43 +0000

    特征的默认方法

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 pub struct Post {
     pub title: String,   // 标题
@@ -7,23 +9,18 @@ pub struct Post {
     pub content: String, // 内容
 }
 
-impl Summary for Post {
-    fn summarize(&self) -> String {
-        format!("文章{}, 作者是{}", self.title, self.author)
-    }
-}
-
 pub struct Weibo {
     pub username: String,
     pub content: String,
 }
+
+impl Summary for Post {}
 
 impl Summary for Weibo {
     fn summarize(&self) -> String {
         format!("{}发表了微博{}", self.username, self.content)
     }
 }
-
 fn main() {
     let post = Post {
         title: "Rust语言简介".to_string(),
