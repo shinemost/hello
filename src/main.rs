154b97c868ec346c3bcacb5b3f03f35485177860
shinemost <supertain147@163.com>
2024-08-15 09:26:05 +0000

    默认实现允许调用相同特征中的其他方法，哪怕这些方法没有默认实现

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
+
 pub struct Post {
     pub title: String,   // 标题
     pub author: String,  // 作者
@@ -14,24 +17,15 @@ pub struct Weibo {
     pub content: String,
 }
 
-impl Summary for Post {}
-
 impl Summary for Weibo {
-    fn summarize(&self) -> String {
-        format!("{}发表了微博{}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 fn main() {
-    let post = Post {
-        title: "Rust语言简介".to_string(),
-        author: "Sunface".to_string(),
-        content: "Rust棒极了!".to_string(),
-    };
     let weibo = Weibo {
         username: "sunface".to_string(),
         content: "好像微博没Tweet好用".to_string(),
     };
-
-    println!("{}", post.summarize());
     println!("{}", weibo.summarize());
 }
