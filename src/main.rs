ef007b1edbe5301dcbd9862e22dcd3efcb85e4f0
shinemost <supertain147@163.com>
2024-08-21 03:23:34 +0000

    函数的返回值约束为特质

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,20 @@ pub struct Weibo {
     pub content: String,
 }
 
+
+pub enum Test{
+    Clubs,
+    Spades,
+    Diamonds,
+    Hearts,
+}
+
+impl Summary for Test{
+    fn summarize_author(&self) ->String{
+        String::from("Test")
+    }
+}
+
 impl Summary for Weibo {
     fn summarize_author(&self) -> String {
         format!("@{}", self.username)
@@ -27,12 +41,31 @@ pub fn notify(item: &impl Summary) {
     println!("Breaking news! {}", item.summarize());
 }
 
+// 函数返回值是实现了Summary特质的类型
+fn returns_summarizable() -> impl Summary {
+    // Post {
+    //     title: String::from("sunface"),
+    //     author: String::from("sunface"),
+    //     content: String::from(
+    //         "m1 max太厉害了，电脑再也不会卡",
+    //     )
+    // }
+    // Weibo{
+    //     username:String::from("weibo"),
+    //     content:String::from(
+    //                 "m2 max太厉害了，电脑再也不会卡",
+    //             )
+    // } 
+    // 也可以返回实现了该特质的枚举
+    Test::Clubs
+
+}
+
+
 fn main() {
-    let weibo = Weibo {
-        username: "sunface".to_string(),
-        content: "好像微博没Tweet好用".to_string(),
-    };
-    notify(&weibo);
-    let s = "123".to_string();
-    notify(&s);
+    let returns_summarizable = returns_summarizable();
+   let summarize = returns_summarizable.summarize_author();
+   println!("{summarize}");
+
+
 }
