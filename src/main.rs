48f459d2ad346d54e47a84f0adebcaafdbe4cf85
shinemost <supertain147@163.com>
2024-08-22 06:26:44 +0000

    特征对象

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,74 @@
 trait Draw {
-    fn draw(&self) -> String;
+    fn draw(&self);
 }
 
-impl Draw for u8 {
-    fn draw(&self) -> String {
-        format!("u8: {}", *self)
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!(
+            "Button with width: {}, height: {}, label: {}",
+            self.width, self.height, self.label
+        );
     }
 }
 
-impl Draw for f64 {
-    fn draw(&self) -> String {
-        format!("f64: {}", *self)
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "SelectBox with width: {}, height: {}",
+            self.width, self.height
+        );
+        for option in &self.options {
+            println!("Option: {}", option);
+        }
     }
 }
 
-// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
-fn draw1(x: Box<dyn Draw>) {
-    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
-    println!("{}", x.draw());
+pub struct Screen {
+    // 特征对象涉及到动态分发，与泛型的静态分发不同
+    // 泛型Rust在编译期间会生成对应的类型编码，而且知道对象的大小
+    // 而特征对象在编译期是不知道的，只有在程序运行时Rust才能通过引用指针知道实际调用哪个类型的方法
+    // 因此会产生动态分发，实际上会影响性能，如果不涉及特征的多个不同实现类型，可直接使用泛型
+    components: Vec<Box<dyn Draw>>,
 }
 
-fn draw2(x: &dyn Draw) {
-    println!("{}", x.draw());
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
 }
 
 fn main() {
-    let x = 1.1f64;
-    // do_something(&x);
-    let y = 8u8;
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
 
-    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw>
-    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
-    draw1(Box::new(x));
-    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
-    draw1(Box::new(y));
-    draw2(&x);
-    draw2(&y);
+    screen.run();
 }
