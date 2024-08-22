cfdd90d0e1e306da62a97dcca3005e27db04fde1
shinemost <supertain147@163.com>
2024-08-22 03:40:17 +0000

    使用特征对象来表达拓展特征的使用范围

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,39 @@
-#![allow(dead_code)]
-
-use std::fmt;
-use std::fmt::Display;
-
-#[derive(Debug, PartialEq)]
-enum FileState {
-    Open,
-    Closed,
+trait Draw {
+    fn draw(&self) -> String;
 }
 
-#[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
-    state: FileState,
-}
-
-impl Display for FileState {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            FileState::Open => write!(f, "OPEN"),
-            FileState::Closed => write!(f, "CLOSED"),
-        }
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
     }
 }
 
-impl Display for File {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<{} ({})>", self.name, self.state)
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
     }
 }
 
-impl File {
-    fn new(name: &str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-            state: FileState::Closed,
-        }
-    }
+// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
+fn draw1(x: Box<dyn Draw>) {
+    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
+    println!("{}", x.draw());
+}
+
+fn draw2(x: &dyn Draw) {
+    println!("{}", x.draw());
 }
 
 fn main() {
-    let f6 = File::new("f6.txt");
-    //...
-    println!("{:?}", f6);
-    println!("{}", f6);
+    let x = 1.1f64;
+    // do_something(&x);
+    let y = 8u8;
+
+    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw>
+    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
+    draw1(Box::new(x));
+    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
+    draw1(Box::new(y));
+    draw2(&x);
+    draw2(&y);
 }
