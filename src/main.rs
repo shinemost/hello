97387d9674b30676776f2b292517ef41bfffdbf7
shinemost <supertain147@163.com>
2024-08-22 07:47:34 +0000

    同名函数的调用

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,31 @@
-use std::ops::Add;
-
-#[derive(Debug, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
+trait Animal {
+    fn baby_name() -> String;
 }
 
-// 由于Add 特征的定义是默认泛型参数是自己，pub trait Add<Rhs = Self>
-// 因此如果是同类型相加则可直接不需要定义泛型类型
-impl Add for Point {
-    type Output = Point;
+struct Dog;
 
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
     }
 }
-#[derive(Debug, PartialEq)]
-struct Millimeters(u32);
-struct Meters(u32);
 
-// 而此实现则是不同类型相加的逻辑，则必须定义泛型的类型
-// 1.减少实现的样板代码
-// 2.扩展类型但是无需大幅修改现有的代码
-impl Add<Meters> for Millimeters {
-    type Output = Millimeters;
-
-    fn add(self, other: Meters) -> Millimeters {
-        Millimeters(self.0 + (other.0 * 1000))
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
 
 fn main() {
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
-    );
+    println!("A baby dog is called a {} by its mom", Dog::baby_name());
 
-    assert_eq!(Millimeters(1) + Meters(10), Millimeters(10001));
+    // println!(
+    //     "A baby dog is called a {} by other animals",
+    //     Animal::baby_name()
+    // );
+    // 使用函数的完全限定语法调用
+    println!(
+        "A baby dog is called a {} by other animals",
+        <Dog as Animal>::baby_name()
+    );
 }
