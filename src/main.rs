ea4ba36b815f18230d99f51a554e7f452fbfccaf
shinemost <supertain147@163.com>
2024-08-23 01:28:07 +0000

    动态数组victor

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use std::fmt;
-
-// 使用元祖结构体，打破孤儿原则，为不在当前作用域的类型实现特质
-struct Wrapper(Vec<String>);
-
-impl fmt::Display for Wrapper {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "[{}]", self.0.join(", "))
-    }
-}
-
 fn main() {
-    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
-    println!("w = {}", w);
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    assert_eq!(v, [1]);
+
+    let mut m = vec![1, 2, 3];
+    m.push(4);
+    assert_eq!(m, [1, 2, 3, 4]);
 }
