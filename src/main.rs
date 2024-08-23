0ccdb28045d91b87288a4bfdef06f082e5b94611
shinemost <supertain147@163.com>
2024-08-23 02:20:37 +0000

    使用特征对象实现victor存储不同类型元素

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-// 使用枚举实现victor存储不同类型元素
-#[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
+trait IpAddr {
+    fn display(&self);
 }
-fn main() {
-    let v = vec![
-        IpAddr::V4("127.0.0.1".to_string()),
-        IpAddr::V6("::1".to_string()),
-    ];
 
-    for ip in v {
-        show_addr(ip)
+struct V4(String);
+// 使用特征对象实现victor存储不同类型元素
+impl IpAddr for V4 {
+    fn display(&self) {
+        println!("ipv4: {:?}", self.0)
+    }
+}
+struct V6(String);
+impl IpAddr for V6 {
+    fn display(&self) {
+        println!("ipv6: {:?}", self.0)
     }
 }
 
-fn show_addr(ip: IpAddr) {
-    println!("{:?}", ip);
+fn main() {
+    let v: Vec<Box<dyn IpAddr>> = vec![
+        Box::new(V4("127.0.0.1".to_string())),
+        Box::new(V6("::1".to_string())),
+    ];
+
+    for ip in v {
+        ip.display();
+    }
 }
