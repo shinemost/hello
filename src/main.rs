64231371131c99d812ab64b37f624f52daf09edc
shinemost <supertain147@163.com>
2024-08-23 03:19:28 +0000

    HashMap的创建

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,33 @@
-// #[derive(Debug)]
-// struct Person {
-//     name: String,
-//     age: u32,
-// }
-
-// impl Person {
-//     fn new(name: String, age: u32) -> Person {
-//         Person { name, age }
-//     }
-// }
-
-// fn main() {
-//     let mut people = vec![
-//         Person::new("Zoe".to_string(), 25),
-//         Person::new("Al".to_string(), 60),
-//         Person::new("John".to_string(), 1),
-//     ];
-//     // 定义一个按照年龄倒序排序的对比函数
-//     people.sort_unstable_by(|a, b| b.age.cmp(&a.age));
-
-//     println!("{:?}", people);
-// }
-
-#[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
-struct Person {
-    name: String,
-    age: u32,
-}
-
-impl Person {
-    fn new(name: String, age: u32) -> Person {
-        Person { name, age }
-    }
-}
+use std::collections::HashMap;
 
 fn main() {
-    let mut people = vec![
-        Person::new("Zoe".to_string(), 25),
-        Person::new("Al".to_string(), 60),
-        Person::new("Al".to_string(), 30),
-        Person::new("John".to_string(), 1),
-        Person::new("John".to_string(), 25),
+    // HashMap
+    let mut map = HashMap::new();
+    map.insert("first", 1);
+    map.insert("second", 2);
+    map.insert("third", 3);
+    for (k, v) in &map {
+        println!("{k},{v}");
+    }
+    // 预先分配空间，减少内存扩容
+    let map: HashMap<&str, i32> = HashMap::with_capacity(10);
+    println!("{}", map.capacity());
+
+    let teams_list = vec![
+        ("中国队".to_string(), 100),
+        ("美国队".to_string(), 10),
+        ("日本队".to_string(), 50),
     ];
 
-    people.sort_unstable();
+    // 手动遍历，手动塞值
+    let mut teams_map = HashMap::new();
+    for team in &teams_list {
+        teams_map.insert(&team.0, team.1);
+    }
 
-    println!("{:?}", people);
+    println!("{:?}", teams_map);
+
+    // 使用迭代器
+    let teams_map2: HashMap<_, _> = teams_list.into_iter().collect();
+    println!("{:?}", teams_map2);
 }
