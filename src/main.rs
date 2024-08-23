6c894bc0b8494b800f2a328b444fdba0c47e1e83
shinemost <supertain147@163.com>
2024-08-23 02:46:17 +0000

    针对结构体的自定义排序方法

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
+// #[derive(Debug)]
+// struct Person {
+//     name: String,
+//     age: u32,
+// }
+
+// impl Person {
+//     fn new(name: String, age: u32) -> Person {
+//         Person { name, age }
+//     }
+// }
+
+// fn main() {
+//     let mut people = vec![
+//         Person::new("Zoe".to_string(), 25),
+//         Person::new("Al".to_string(), 60),
+//         Person::new("John".to_string(), 1),
+//     ];
+//     // 定义一个按照年龄倒序排序的对比函数
+//     people.sort_unstable_by(|a, b| b.age.cmp(&a.age));
+
+//     println!("{:?}", people);
+// }
+
+#[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    fn new(name: String, age: u32) -> Person {
+        Person { name, age }
+    }
+}
+
 fn main() {
-    let mut vec = vec![1, 5, 10, 2, 15];
-    vec.sort_unstable();
-    assert_eq!(vec, vec![1, 2, 5, 10, 15]);
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("Al".to_string(), 30),
+        Person::new("John".to_string(), 1),
+        Person::new("John".to_string(), 25),
+    ];
 
-    // let mut vec = vec![1.0, 5.6, 10.3, 2.0, 15f32];
-    // vec.sort_unstable();
-    // assert_eq!(vec, vec![1.0, 2.0, 5.6, 10.3, 15f32]);
+    people.sort_unstable();
 
-    let mut vec = vec![1.0, 5.6, 10.3, 2.0, 15f32];
-    vec.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
-    assert_eq!(vec, vec![1.0, 2.0, 5.6, 10.3, 15f32]);
+    println!("{:?}", people);
 }
