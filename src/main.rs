124f7f7c8a5de83560e5a23f7c4bf7d68985f903
shinemost <supertain147@163.com>
2024-08-27 02:49:50 +0000

    Result的unwarp函数

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => panic!("Problem opening the file: {:?}", other_error),
-        },
-    };
+    //使用Result的unwarp函数，如果Some有值，直接返回，如果没有值，直接panic
+    let _f = File::open("hello.txt").unwrap();
 }
