b0c6485b7fa737d6b9c84b248204cf9cbebdb7ef
shinemost <supertain147@163.com>
2024-08-30 09:22:43 +0000

    ?也适用于option

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::fs;
-use std::io;
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
+fn first(arr: &[i32]) -> Option<&i32> {
+    // ?也能适用于Option,提前返回None
+    let v = arr.get(6)?;
+    Some(v)
 }
 
 fn main() {
-    // 错误传播
-    let r = read_username_from_file().expect("出现异常了，但是我不告诉你真正原因");
+    let a = [1, 2, 3, 4, 5];
+    let r = first(&a);
     println!("{:?}", r);
 }
