722f6899d71417d0388f3b43f075560eb5059165
shinemost <supertain147@163.com>
2024-08-30 09:11:54 +0000

    ?链式调用

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::fs::File;
+use std::io;
+use std::io::Read;
 
-fn open_file() -> Result<File, Box<dyn std::error::Error>> {
-    // ？可以进行隐式转换，自动将std::io::Error转换成了std::error::Error
-    // 因为std::io::Error实现了std::error::Error
-    let f = File::open("hello.txt")?;
-    Ok(f)
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    // ？也可以链式调用，前面返回OK，直接传入后一个函数进行使用
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
 }
 
 fn main() {
     // 错误传播
-    let r = open_file();
+    let r = read_username_from_file().expect("出现异常了，但是我不告诉你真正原因");
     println!("{:?}", r);
 }
