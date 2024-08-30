d82f98b476fb799356c9edacfbe6062afe065006
shinemost <supertain147@163.com>
2024-08-30 09:16:02 +0000

    没有最短，只有更短

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::fs::File;
+use std::fs;
 use std::io;
-use std::io::Read;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut s = String::new();
-
-    // ？也可以链式调用，前面返回OK，直接传入后一个函数进行使用
-    File::open("hello.txt")?.read_to_string(&mut s)?;
-
-    Ok(s)
+    fs::read_to_string("hello.txt")
 }
 
 fn main() {
