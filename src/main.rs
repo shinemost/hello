3f4e726301eab1d0259666a0bad516eb46899713
shinemost <supertain147@163.com>
2024-08-30 08:39:42 +0000

    expect

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 
 fn main() {
-    //使用Result的unwarp函数，如果Some有值，直接返回，如果没有值，直接panic
-    let _f = File::open("hello.txt").unwrap();
+    //类似unwarp但是可以打印预留的错误信息
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
 }
