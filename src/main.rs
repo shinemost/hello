aa990fd367fb1cd4e91fcb95ad5f22893202984d
shinemost <supertain147@163.com>
2024-08-30 09:26:44 +0000

    ？针对Option的链式调用

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn first(arr: &[i32]) -> Option<&i32> {
-    // ?也能适用于Option,提前返回None
-    let v = arr.get(6)?;
-    Some(v)
+    arr.get(6)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
 
 fn main() {
     let a = [1, 2, 3, 4, 5];
     let r = first(&a);
     println!("{:?}", r);
+
+    let s = "";
+    let m = last_char_of_first_line(&s).expect("空字符串");
+    println!("{m}");
 }
