58756a2cb25e8c9d4e84e4c862808df0af0ceb0a
shinemost <supertain147@163.com>
2024-08-30 08:55:52 +0000

    补充注释

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::{self, Read};
 
 fn read_username_from_file() -> Result<String, io::Error> {
+    // 这个相比于层层手动往上抛异常，也太方便了
+    // 想想java的throw
     let mut f = File::open("hello.txt")?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
