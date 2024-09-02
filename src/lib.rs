0fc9adaea183f54b63f5fe2fce24d12ab8fd580f
shinemost <supertain147@163.com>
2024-09-02 08:18:58 +0000

    测试用例

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+// in lib.rs
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+// 类似于java的@SpringBootTest
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // 类似于java的@Test注解
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
