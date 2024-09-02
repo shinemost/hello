2974ee272f859742d9689b3ab74b27a563a32e85
shinemost <supertain147@163.com>
2024-09-02 11:12:17 +0000

    重定向错误信息输出

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
 
     if let Err(e) = hello::run(config) {
         // --snip--
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
