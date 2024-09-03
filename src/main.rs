16748928a7a9a8b978befde51b85b3355d6e7809
shinemost <supertain147@163.com>
2024-09-03 21:37:33 +0800

    生命周期例子

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
-fn f<'a, T>(x: *const T) -> &'a T {
-    unsafe {
-        // 此时野指针凭空产生a生命周期,
-        &*x
+fn main() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+
+        result = longest(string1.as_str(), string2.as_str());
+    } // 到这里string2生命周期也结束了
+    // result的生命周期到这里
+    // string2的生命周期也应该到这里
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
-
-// b>=a
-struct DoubleRef<'a, 'b: 'a, T> {
-    r: &'a T,
-    s: &'b T,
-}
-// a<=T
-struct Ref<'a, T> {
-    r: &'a T,
-}
-
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
-
-// 此处必须指定a:b slef.part的生命周期是a
-// 而方法的返回值生命周期是b
-// 只有a>=b才能将a转为b
-impl<'a: 'b, 'b> ImportantExcerpt<'a> {
-    fn announce_and_return_part(&'a self, announcement: &'b str) -> &'b str {
-        println!("Attention please: {}", announcement);
-        self.part
-    }
-}
-
-
-fn main() {}
