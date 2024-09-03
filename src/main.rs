0623365860823e26ec3527d76be1d8b383045467
shinemost <supertain147@163.com>
2024-09-03 21:28:15 +0800

    生命周期比较

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,29 @@ fn f<'a, T>(x: *const T) -> &'a T {
     }
 }
 
+// b>=a
+struct DoubleRef<'a, 'b: 'a, T> {
+    r: &'a T,
+    s: &'b T,
+}
+// a<=T
+struct Ref<'a, T> {
+    r: &'a T,
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+// 此处必须指定a:b slef.part的生命周期是a
+// 而方法的返回值生命周期是b
+// 只有a>=b才能将a转为b
+impl<'a: 'b, 'b> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&'a self, announcement: &'b str) -> &'b str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+
 fn main() {}
