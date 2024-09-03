427efd7f9e7f14af4ea887bd1fa3c5cbf2d4e267
shinemost <supertain147@163.com>
2024-09-03 21:00:59 +0800

    无界生命周期

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-#![allow(unused)]
-fn main() {
-    use std::collections::HashMap;
-    use std::hash::Hash;
-    fn get_default<'m, K, V>(map: &'m mut HashMap<K, V>, key: K) -> &'m mut V
-    where
-        K: Clone + Eq + Hash,
-        V: Default,
-    {
-        // 此处获取key之后，对map的可变借用就应该结束了
-        // 但是由于编译器不够智能，会扩大map的可变借用的作用域范围，直到整个match语句结束
-
-        match map.get_mut(&key) {
-            Some(value) => value,
-            None => {
-                // 造成在此处又发生了map的可变借用，发生报错
-                map.insert(key.clone(), V::default());
-                map.get_mut(&key).unwrap()
-            }
-        }
+fn f<'a, T>(x: *const T) -> &'a T {
+    unsafe {
+        // 此时野指针凭空产生a生命周期,
+        &*x
     }
 }
 
+fn main() {}
