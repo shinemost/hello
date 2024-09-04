7f5832aeb7ee8b43530bb6aa712f1ad3d0fcee55
付海静 <fuhaijing@gotion.com.cn>
2024-09-04 15:22:19 +0800

    闭包能获取作用域里的变量

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
-struct Cacher<T, E>
-where
-    T: Fn(E) -> E,
-    E: Copy,
-{
-    query: T,
-    value: Option<E>,
-}
+fn main() {
+  let x = 4;
 
-impl<T, E> Cacher<T, E>
-where
-    T: Fn(E) -> E,
-    E: Copy,
-{
-    fn new(query: T) -> Cacher<T, E> {
-        Cacher { query, value: None }
-    }
+  let equal_to_x = |z| z == x;
 
-    fn value(&mut self, arg: E) -> E {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.query)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-fn main() {}
+  let y = 4;
 
-#[test]
-fn call_with_different_values() {
-    let mut c = Cacher::new(|a| a);
-
-    let v1 = c.value(1);
-    let v2 = c.value(2);
-
-    assert_eq!(v2, 1);
+  assert!(equal_to_x(y));
 }
