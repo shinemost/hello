42516442dfc4898397371e248b83067c86108bd9
shinemost <supertain147@163.com>
2024-09-08 10:24:29 +0800

    迭代器其二

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,44 @@
+use std::collections::HashMap;
+
 fn main() {
-    // 数组实现了IntoIterator特征，所以可以直接迭代
-    let arr = [1, 2, 3];
-    for v in arr {
-        println!("{}", v);
-    }
+    let v1 = vec![1, 2, 3];
 
-    // IntoIterator特征有一个into_inter方法，可以显式的将数组转为迭代器
-    // 该方法会交出所有权, 调用此方法后无法再使用原变量,除非实现了copy特征
-    // 但是数组会特别，需要查看文档。
-    for m in arr.into_iter() {
-        println!("{}", m);
-    }
-    println!("{:?}", arr);
+    let v1_iter = v1.iter();
 
-    // 直接对数值序列进行迭代，也是很常见的使用方式。
-    for ss in 0..10 {
-        println!("{}", ss);
-    }
+    let total: i32 = v1_iter.sum();
 
-    let s = vec![4, 5, 6];
-    for ss in s {
-        println!("{ss}");
-    }
+    assert_eq!(total, 6);
 
-    let arr = [1, 2, 3];
-    let mut arr_iter = arr.into_iter();
+    // v1_iter 是借用了 v1，因此 v1 可以照常使用
+    println!("{:?}", v1);
 
-    assert_eq!(arr_iter.next(), Some(1));
-    assert_eq!(arr_iter.next(), Some(2));
-    assert_eq!(arr_iter.next(), Some(3));
-    assert_eq!(arr_iter.next(), None);
+    // 以下代码会报错，因为 `sum` 拿到了迭代器 `v1_iter` 的所有权
+    // println!("{:?}", v1_iter);
 
-    let values = vec![10, 20, 30];
-    // 使用迭代器模拟for循环
-    // for循环其实就是迭代器的语法糖
-    {
-        let result = match IntoIterator::into_iter(values) {
-            mut iter => loop {
-                match iter.next() {
-                    Some(x) => { println!("{}", x); }
-                    None => break,
-                }
-            },
-        };
-        result
-    }
+    let v1: Vec<i32> = vec![1, 2, 3];
 
-    //     Iterator本身也实现了IntoIterator特征
-    // Vec没有实现copy特征，所有权被移除，这里报错
-    // values.into_iter();
-    for v in arr.into_iter().into_iter().into_iter().into_iter().into_iter() {
-        println!("{}", v)
-    }
+    // 这里的 map 方法是一个迭代者适配器，它是惰性的，不产生任何行为，因此我们还需要一个消费者适配器进行收尾
+    // v1.iter().map(|x| x + 1);
+    // collect就是一个消费者适配器
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
-    //     转换迭代器的三个方法：
-    //     into_iterator 会夺取所有权
-    //     iter 不可变引用
-    //     iter_mut 可变引用
-    let values = vec![1, 2, 3];
-    let _values_iter = values.iter();
-    println!("{:?}", values);
+    assert_eq!(v2, vec![2, 3, 4]);
 
-    let mut values = vec![1, 2, 3];
-    let mut_values_iter = values.iter_mut();
-    mut_values_iter.for_each(|x| *x *= 2);
-    println!("{:?}", values);
+    // zip将两个迭代器压成一个迭代器，迭代器里的元素是元祖形式，第一个元素来自第一个迭代器，第二个元素来自第二个迭代器
+    let names = ["sunface", "sunfei"];
+    let ages = [18, 18];
+    let folks: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
 
-    //     Iterator特征和IntoIterator特征的区别
-    //     实现了Iterator特征的类型都可以称为迭代器，才能调用next
-    //     IntoIterator 强调的是某一个类型如果实现了该特征，它可以通过 into_iter，iter 等方法变成一个迭代器
+    println!("{:?}", folks);
+}
 
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+// 使用闭包很方便，可以捕获环境值，这里的shoe_size就是
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
