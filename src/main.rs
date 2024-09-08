7be6f2a271c6b83879556ae8a1e14e0f5f6550ff
shinemost <supertain147@163.com>
2024-09-08 11:44:04 +0800

    迭代器其三

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,59 @@
-use std::collections::HashMap;
+mod lib;
 
 fn main() {
-    let v1 = vec![1, 2, 3];
+    let mut counter = Counter::new();
 
-    let v1_iter = v1.iter();
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
 
-    let total: i32 = v1_iter.sum();
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
 
-    assert_eq!(total, 6);
+    // enumerate是迭代器消费者，会返回一个新的迭代器，元素是索引，值的元祖
+    let v = vec![1u64, 2, 3, 4, 5, 6];
+    let val = v.iter()
+        .enumerate()
+        // 每两个元素剔除一个
+        // [1, 3, 5]
+        .filter(|&(idx, _)| idx % 2 == 0)
+        .map(|(_, val)| val)
+        // 累加 1+3+5 = 9
+        .fold(0u64, |sum, acm| sum + acm);
 
-    // v1_iter 是借用了 v1，因此 v1 可以照常使用
-    println!("{:?}", v1);
-
-    // 以下代码会报错，因为 `sum` 拿到了迭代器 `v1_iter` 的所有权
-    // println!("{:?}", v1_iter);
-
-    let v1: Vec<i32> = vec![1, 2, 3];
-
-    // 这里的 map 方法是一个迭代者适配器，它是惰性的，不产生任何行为，因此我们还需要一个消费者适配器进行收尾
-    // v1.iter().map(|x| x + 1);
-    // collect就是一个消费者适配器
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
-
-    assert_eq!(v2, vec![2, 3, 4]);
-
-    // zip将两个迭代器压成一个迭代器，迭代器里的元素是元祖形式，第一个元素来自第一个迭代器，第二个元素来自第二个迭代器
-    let names = ["sunface", "sunfei"];
-    let ages = [18, 18];
-    let folks: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
-
-    println!("{:?}", folks);
+    println!("{}", val);
 }
 
-struct Shoe {
-    size: u32,
-    style: String,
+struct Counter {
+    count: u32,
 }
 
-// 使用闭包很方便，可以捕获环境值，这里的shoe_size就是
-fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
 }
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+
+
 
