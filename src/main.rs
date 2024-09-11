fe05e91be74e11e766db6002076f49d6a6d46799
shinemost <supertain147@163.com>
2024-09-11 23:13:06 +0800

    迭代器适配者-flat_map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::str::FromStr;
+use std::collections::HashMap;
+
 fn main() {
-    let text = "1\nfrond .25 289\n3.1415 estuary\n";
-    for number in text.split_whitespace().filter_map(|x| f64::from_str(x).ok()) {
-        println!("{:4.2} ", number);
-    }
-    //     等价于下面代码
-    for number in text.split_whitespace()
-        .map(|x1| f64::from_str(x1))
-        .filter(|r| r.is_ok())
-        .map(|r| r.unwrap()) {
-        println!("{:4.2} ", number);
+    let mut country_major_cities = HashMap::new();
+    country_major_cities.insert("Japan", vec!["Tokyo", "Kyoto"]);
+    country_major_cities.insert("US", vec!["Portland", "Nashville"]);
+    country_major_cities.insert("Brazil", vec!["Sao Paulo", "Brasilia"]);
+    country_major_cities.insert("Kenya", vec!["Nairobi", "Mombasa"]);
+
+    // flat_map返回任意种类的可迭代者
+    let countries = ["Japan", "US", "Brazil", "Kenya"];
+    for &city in countries.iter().flat_map(|country| &country_major_cities[country]) {
+        println!("{}", city);
     }
 }
