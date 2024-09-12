d53687bdf590dda85092d657246a77d73ebb5741
付海静 <fuhaijing@gotion.com.cn>
2024-09-12 07:59:16 +0000

    迭代器适配器-inspect

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 fn main() {
-    let bee_parts = ["head", "thorax", "abdomen"];
-    let mut iter = bee_parts.iter();
+    // inspect 探查，一般用于Debug程序看看是否按照预想的运行，或者打印错误日志
+    let upper_case: String = "groβe"
+        .chars()
+        .inspect(|c| println!("before: {:?}", c))
+        .flat_map(|c| c.to_uppercase())
+        .inspect(|c| println!("after: {:?}", c))
+        .collect();
+    assert_eq!(upper_case, "GROΒE");
 
-    // 实现了DoubleEndedIterator特征的迭代器都可以从两端遍历
-    assert_eq!(iter.next(), Some(&"head"));
-    assert_eq!(iter.next_back(), Some(&"abdomen"));
-    assert_eq!(iter.next(), Some(&"thorax"));
-    assert_eq!(iter.next_back(), None);
-    assert_eq!(iter.next(), None);
+    // Debug程序，此处只有偶数才能进入第二个inspect
+    let a = [1, 4, 2, 3];
+    let sum = a
+        .iter()
+        .cloned()
+        .inspect(|x| println!("about to filter: {x}"))
+        .filter(|x| x % 2 == 0)
+        .inspect(|x| println!("made it through filter: {x}"))
+        .fold(0, |sum, i| sum + i);
 
-    //对于实现了DoubleEndedIterator特征的迭代器调用rev函数进行反转
-    let meals = ["breakfast", "lunch", "dinner"];
-    let mut iter = meals.iter().rev();
-    assert_eq!(iter.next(), Some(&"dinner"));
-    assert_eq!(iter.next_back(), Some(&"breakfast"));
-    assert_eq!(iter.next(), Some(&"lunch"));
-    assert_eq!(iter.next_back(), None);
-    assert_eq!(iter.next(), None);
+    println!("{sum}");
+
+    // 在丢弃error前记录日志
+    let lines = ["1", "2", "a"];
+
+    let sum: i32 = lines
+        .iter()
+        .map(|line| line.parse::<i32>())
+        .inspect(|num| {
+            if let Err(ref e) = *num {
+                println!("Parsing error: {e}");
+            }
+        })
+        .filter_map(Result::ok)
+        .sum();
+
+    println!("Sum: {sum}");
 }
