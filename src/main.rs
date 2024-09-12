446ede7f7cdbb732d1d2acf63809b14779da55b4
付海静 <fuhaijing@gotion.com.cn>
2024-09-12 02:58:22 +0000

    迭代器适配器-rev以及DoubleEndedIterator特征

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-struct Flaky(bool);
-
-impl Iterator for Flaky {
-    type Item = &'static str;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.0 {
-            self.0 = false;
-            Some("totally the last item")
-        } else {
-            self.0 = true;
-            None
-        }
-    }
-}
-
 fn main() {
-    let mut flaky = Flaky(true);
-    assert_eq!(flaky.next(), Some("totally the last item"));
-    assert_eq!(flaky.next(), None);
-    assert_eq!(flaky.next(), Some("totally the last item"));
-    assert_eq!(flaky.next(), None);
+    let bee_parts = ["head", "thorax", "abdomen"];
+    let mut iter = bee_parts.iter();
 
-    // fuse保险丝，确保第一次返回None，后续调用都返回None
-    let mut not_flaky = Flaky(true).fuse();
+    // 实现了DoubleEndedIterator特征的迭代器都可以从两端遍历
+    assert_eq!(iter.next(), Some(&"head"));
+    assert_eq!(iter.next_back(), Some(&"abdomen"));
+    assert_eq!(iter.next(), Some(&"thorax"));
+    assert_eq!(iter.next_back(), None);
+    assert_eq!(iter.next(), None);
 
-    assert_eq!(not_flaky.next(), Some("totally the last item"));
-    assert_eq!(not_flaky.next(), None);
-    assert_eq!(not_flaky.next(), None);
-    assert_eq!(not_flaky.next(), None);
+    //对于实现了DoubleEndedIterator特征的迭代器调用rev函数进行反转
+    let meals = ["breakfast", "lunch", "dinner"];
+    let mut iter = meals.iter().rev();
+    assert_eq!(iter.next(), Some(&"dinner"));
+    assert_eq!(iter.next_back(), Some(&"breakfast"));
+    assert_eq!(iter.next(), Some(&"lunch"));
+    assert_eq!(iter.next_back(), None);
+    assert_eq!(iter.next(), None);
 }
