23f3899428e4e2f36421cf9334248ddbd69c1967
付海静 <fuhaijing@gotion.com.cn>
2024-09-12 09:03:36 +0000

    消耗迭代器-sum,count,product

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use std::iter::{once, repeat};
-
 fn main() {
-    // 儿童文字游戏，1-100 被3整除的数替换成fizz，被5整除的数替换成buzz,被3与5都能整除的数替换成fizzbuzz
-    let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
-    let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
-    let fizzes_buzzes = fizzes.zip(buzzes);
+    // 求和
+    let s: u64 = (1..=100).sum();
+    println!("{s}");
 
-    let fizz_buzz = (1..100).zip(fizzes_buzzes).map(|tuple| match tuple {
-        (i, ("", "")) => i.to_string(),
-        (_, (fizz, buzz)) => format!("{}{}", fizz, buzz),
-    });
-    for line in fizz_buzz {
-        println!("{line}");
-    }
+    // 个数 返回的是usize
+    let s: usize = (1..=100).count();
+    println!("{s}");
+
+    // 乘积
+    let s: u64 = (1..=20).product();
+    println!("{s}");
 }
