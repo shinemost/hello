7dc0ac4b84ecdb0240ae0d0805403849fee38b8f
付海静 <fuhaijing@gotion.com.cn>
2024-09-13 06:46:27 +0000

    模拟在遍历时删除特定元素发生的错误

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-
 fn main() {
-    let mut rng = thread_rng();
-    let mut y = [1, 2, 3, 4, 5];
-    println!("Unshuffled: {:?}", y);
-    y.shuffle(&mut rng);
-    println!("Shuffled:   {:?}", y);
+    // 同时存在了可变引用和不可变引用
+    let mut y = vec![1, 2, 3, 4, 5];
+    // for (index, &val) in y.iter().enumerate() {
+    //     if val > 4 {
+    //         y.remove(index);
+    //     }
+    // }
+
+    y.retain(|&x| x <= 4);
+    assert_eq!(y, vec![1, 2, 3, 4]);
 }
