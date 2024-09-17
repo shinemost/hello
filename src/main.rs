98747d037162398df8cdbd41dc0f630da6ba7498
shinemost <supertain147@163.com>
2024-09-17 12:03:16 +0800

    Cell::from_mut与as_slice_of_cells方法使用

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::cell::Cell;
+
+fn retain_even(nums: &mut Vec<i32>) {
+    // 使用Cell的两个方法方便的将&mut [T] 类型转换成 &[Cell<T>] 类型
+    let slice: &[Cell<i32>] = Cell::from_mut(&mut nums[..])
+        .as_slice_of_cells();
+
+    let mut i = 0;
+    for num in slice.iter().filter(|num| is_even(num.get())) {
+        slice[i].set(num.get());
+        i += 1;
+    }
+
+    nums.truncate(i);
+}
+fn is_even(i: i32) -> bool {
+    i % 2 == 0
+}
 fn main() {
-    let s = Rc::new(RefCell::new("我很善变，还拥有多个主人".to_string()));
-
-    let s1 = s.clone();
-    let s2 = s.clone();
-    // let mut s2 = s.borrow_mut();
-    s2.borrow_mut().push_str(", oh yeah!");
-
-    println!("{:?}\n{:?}\n{:?}", s, s1, s2);
+    let mut nums: Vec<i32> = vec![1, 2, 3, 4];
+    println!("nums: {:?}", nums);
+    retain_even(&mut nums);
+    println!("nums: {:?}", nums);
 }
