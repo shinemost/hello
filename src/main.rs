d6f7ae0f768e21f1b0ced1bbace6b22079c2ad1c
shinemost <supertain147@163.com>
2024-09-17 11:39:37 +0800

    Rc与RefCell结合在一起使用

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
+use std::rc::Rc;
 fn main() {
-    // 要求value 必须实现copy特征
-    let c = Cell::new("asdf");
-    // let c = Cell::new(String::from("Hello"));
-    let one = c.get();
-    c.set("qwer");
-    let two = c.get();
-    println!("{},{}", one, two);
+    let s = Rc::new(RefCell::new("我很善变，还拥有多个主人".to_string()));
 
-    // Refcell没有本质改变所有权规则，只是将校验延迟了，能够通过编译，但是运行时会报Panic
-    let s = RefCell::new(String::from("hello, world"));
-    let s1 = s.borrow();
-    let s2 = s.borrow_mut();
-    println!("{},{}", s1, s2);
+    let s1 = s.clone();
+    let s2 = s.clone();
+    // let mut s2 = s.borrow_mut();
+    s2.borrow_mut().push_str(", oh yeah!");
 
-    // 与 Cell 用于可 Copy 的值不同，RefCell 用于引用
-    // RefCell 只是将借用规则从编译期推迟到程序运行期，并不能帮你绕过这个规则
-    // RefCell 适用于编译期误报或者一个引用被在多处代码使用、修改以至于难于管理借用关系时
-    // 使用 RefCell 时，违背借用规则会导致运行期的 panic
+    println!("{:?}\n{:?}\n{:?}", s, s1, s2);
 }
