338347a779411d3eea5941052ffcc28a34fdc6b1
付海静 <fuhaijing@gotion.com.cn>
2024-09-18 02:45:02 +0000

    结构体自引用例子

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,51 @@
-use std::cell::RefCell;
-use std::rc::{Rc, Weak};
+// 自引用结构体
+struct SelfRef<'a> {
+    value: String,
 
-#[derive(Debug)]
-struct Node {
-    value: i32,
-    parent: RefCell<Weak<Node>>,
-    children: RefCell<Vec<Rc<Node>>>,
+    // 该引用指向上面的value
+    pointer_to_value: &'a str,
 }
 
-fn main() {
-    let leaf = Rc::new(Node {
-        value: 3,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![]),
-    });
+#[derive(Debug)]
+struct WhatAboutThis<'a> {
+    name: String,
+    nickname: Option<&'a str>,
+}
 
-    println!(
-        "leaf strong = {}, weak = {}",
-        Rc::strong_count(&leaf),
-        Rc::weak_count(&leaf),
-    );
+// fn creator<'a>() -> WhatAboutThis<'a> {
+//     let mut tricky = WhatAboutThis {
+//         name: "Annabelle".to_string(),
+//         nickname: None,
+//     };
+//     tricky.nickname = Some(&tricky.name[..4]);
 
-    {
-        let branch = Rc::new(Node {
-            value: 5,
-            parent: RefCell::new(Weak::new()),
-            children: RefCell::new(vec![Rc::clone(&leaf)]),
-        });
-
-        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-        println!(
-            "branch strong = {}, weak = {}",
-            Rc::strong_count(&branch),
-            Rc::weak_count(&branch),
-        );
-
-        println!(
-            "leaf strong = {}, weak = {}",
-            Rc::strong_count(&leaf),
-            Rc::weak_count(&leaf),
-        );
+//     tricky
+// }
+impl<'a> WhatAboutThis<'a> {
+    fn tie_the_knot(&'a mut self) {
+        self.nickname = Some(&self.name[..4]);
     }
-    // branch离开起作用域已经被销毁了，所以此处是None
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-    println!(
-        // 引用计数也变成1
-        "leaf strong = {}, weak = {}",
-        Rc::strong_count(&leaf),
-        Rc::weak_count(&leaf),
-    );
+}
+fn main() {
+    // let s = "aaa".to_string();
+    // let v = SelfRef {
+    //     value: s,
+    //     pointer_to_value: &s,
+    // };
+
+    // let mut tricky = WhatAboutThis {
+    //     name: "Annabelle".to_string(),
+    //     nickname: None,
+    // };
+    // tricky.nickname = Some(&tricky.name[..4]);
+
+    // println!("{:?}", tricky);
+
+    let mut tricky = WhatAboutThis {
+        name: "Annabelle".to_string(),
+        nickname: None,
+    };
+    tricky.tie_the_knot();
+
+    println!("{:?}", tricky);
 }
