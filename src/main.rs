df2ec0b733d7558623055adcf60db48517b034e5
付海静 <fuhaijing@gotion.com.cn>
2024-09-18 06:37:27 +0000

    使用lazy_static三方库以及OnceLock进行运行时初始化

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,58 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
-
-// 定义常量
-// 关键字是const而不是let
-// 定义常量必须指明类型（如 i32）不能省略
-// 定义常量时变量的命名规则一般是全部大写
-// 常量可以在任意作用域进行定义，其生命周期贯穿整个程序的生命周期。编译时编译器会尽可能将其内联到代码中，所以在不同地方对同一常量的引用并不能保证引用到相同的内存地址
-// 常量的赋值只能是常量表达式/数学表达式，也就是说必须是在编译期就能计算出的值，如果需要在运行时才能得出结果的值比如函数，则不能赋值给常量表达式
-// 对于变量出现重复的定义(绑定)会发生变量遮盖，后面定义的变量会遮住前面定义的变量，常量则不允许出现重复的定义
-const MAX_ID: usize = usize::MAX / 2;
-// 静态变量
-// 静态变量不会被内联，在整个程序中，静态变量只有一个实例，所有的引用都会指向同一个地址
-// 存储在静态变量中的值必须要实现 Sync trait
-static mut REQUEST_RECV: usize = 0;
-
-static REQUEST_RECV_ATOM: AtomicUsize = AtomicUsize::new(0);
-
-struct Factory {
-    factory_id: usize,
+use std::sync::{Mutex, OnceLock};
+//  在静态上下文中，调用被限制为常量函数、元组结构体和元组变体。
+// static NAMES: Mutex<String> = Mutex::new(String::from("Sunface, Jack, Allen"));
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+// lazy_static是社区提供的非常强大的宏，用于懒初始化静态变量，之前的静态变量都是在编译期初始化的，
+// 因此无法使用函数调用进行赋值，而lazy_static允许我们在运行期初始化静态变量！
+lazy_static! {
+    static ref NAMES: Mutex<String> = Mutex::new(String::from("Sunface, Jack, Allen"));
+    static ref HASHMAP: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(0, "foo");
+        m.insert(1, "bar");
+        m.insert(2, "baz");
+        m
+    };
 }
 
-static GLOBAL_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
-
-fn generate_id() -> usize {
-    // 检查两次溢出，否则直接加一可能导致溢出
-    let current_val = GLOBAL_ID_COUNTER.load(Ordering::Relaxed);
-    if current_val > MAX_ID {
-        panic!("Factory ids overflowed");
-    }
-    GLOBAL_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-    let next_id = GLOBAL_ID_COUNTER.load(Ordering::Relaxed);
-    if next_id > MAX_ID {
-        panic!("Factory ids overflowed");
-    }
-    next_id
+// 现在可以直接使用标准库的std::sync::OnceLock达成相同的结果
+fn hashmap() -> &'static HashMap<u32, &'static str> {
+    static HASHMAP: OnceLock<HashMap<u32, &str>> = OnceLock::new();
+    HASHMAP.get_or_init(|| {
+        let mut m = HashMap::new();
+        m.insert(0, "foo");
+        m.insert(1, "bar");
+        m.insert(2, "baz");
+        m
+    })
 }
 
-impl Factory {
-    fn new() -> Self {
-        Self {
-            factory_id: generate_id(),
-        }
-    }
+fn mutex() -> &'static Mutex<String> {
+    static NAMES: OnceLock<Mutex<String>> = OnceLock::new();
+    NAMES.get_or_init(|| {
+        let m = Mutex::new(String::from("Sunface, Jack, Allen"));
+        m
+    })
 }
 
 fn main() {
-    println!("用户ID允许的最大值是{}", MAX_ID);
-    // Rust 要求必须使用unsafe语句块才能访问和修改static变量，因为这种使用方式往往并不安全，其实编译器是对的，当在多线程中同时去修改时，会不可避免的遇到脏数据。
-    unsafe {
-        REQUEST_RECV += 1;
-        assert_eq!(REQUEST_RECV, 1);
-    };
+    let v = NAMES.lock().unwrap();
+    println!("{}", v);
 
-    for _ in 0..100 {
-        REQUEST_RECV_ATOM.fetch_add(1, Ordering::Relaxed);
-    }
+    let mut v = mutex().lock().unwrap();
+    v.push_str(", Myth");
+    println!("{}", v);
 
-    println!("当前用户请求数{:?}", REQUEST_RECV_ATOM);
+    // 首次访问`HASHMAP`的同时对其进行初始化
+    println!("The entry for `0` is \"{}\".", HASHMAP.get(&0).unwrap());
 
-    let facotroy1 = Factory::new();
-    let facotroy2 = Factory::new();
-    println!("{}", facotroy1.factory_id);
-    println!("{}", facotroy2.factory_id);
+    // 后续的访问仅仅获取值，再不会进行任何初始化操作
+    println!("The entry for `1` is \"{}\".", HASHMAP.get(&1).unwrap());
+
+    // 首次访问`HASHMAP`的同时对其进行初始化
+    println!("The entry for `0` is \"{}\".", hashmap().get(&0).unwrap());
+
+    // 后续的访问仅仅获取值，再不会进行任何初始化操作
+    println!("The entry for `1` is \"{}\".", hashmap().get(&1).unwrap());
 }
