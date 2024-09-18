75a5959cee75465d25dc6a14abf151cf6759964a
付海静 <fuhaijing@gotion.com.cn>
2024-09-18 07:01:07 +0000

    标准库OnceLock例子

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,51 @@
-#[derive(Debug)]
-struct Config {
-    a: String,
-    b: String,
-}
-static mut CONFIG: Option<&mut Config> = None;
+// 低于Rust 1.70版本中， OnceCell 和 SyncOnceCell 的API为实验性的 ，
+// 需启用特性 `#![feature(once_cell)]`。
+// #![feature(once_cell)]
+// use std::{lazy::SyncOnceCell, thread};
 
-fn init() -> Option<&'static mut Config> {
-    // Some(&mut Config {
-    //     a: "A".to_string(),
-    //     b: "B".to_string(),
-    // })
-
-    let c = Box::new(Config {
-        a: "A".to_string(),
-        b: "B".to_string(),
-    });
-
-    Some(Box::leak(c))
-}
+// Rust 1.70版本以上,
+use std::{sync::OnceLock, thread};
 
 fn main() {
-    // unsafe {
-    //     // 试图将局部变量Config赋值给全局变量CONFIG
-    //     CONFIG = Some(&mut Config {
-    //         a: "A".to_string(),
-    //         b: "B".to_string(),
-    //     });
-    //     // Config被回收
-
-    //     println!("{:?}", CONFIG)
-    // }
-
-    let c = Box::new(Config {
-        a: "A".to_string(),
-        b: "B".to_string(),
+    // 子线程中调用
+    let handle = thread::spawn(|| {
+        let logger = Logger::global();
+        logger.log("thread message".to_string());
     });
 
-    unsafe {
-        // 将`c`从内存中泄漏，变成`'static`生命周期，和程序一样久，就可以赋值给全局静态变量了
-        CONFIG = Some(Box::leak(c));
-        println!("{:?}", CONFIG);
+    // 主线程调用
+    let logger = Logger::global();
+    logger.log("some message".to_string());
+
+    let logger2 = Logger::global();
+    logger2.log("other message".to_string());
+
+    handle.join().unwrap();
+}
+
+#[derive(Debug)]
+struct Logger;
+
+// 低于Rust 1.70版本
+// static LOGGER: SyncOnceCell<Logger> = SyncOnceCell::new();
+
+// Rust 1.70版本以上
+static LOGGER: OnceLock<Logger> = OnceLock::new();
+
+impl Logger {
+    fn global() -> &'static Logger {
+        // 获取或初始化 Logger
+        LOGGER.get_or_init(|| {
+            println!("Logger is being created..."); // 初始化打印
+            Logger
+        })
     }
 
-    unsafe {
-        CONFIG = init();
-
-        println!("{:?}", CONFIG)
+    fn log(&self, message: String) {
+        println!("{}", message)
     }
 }
+
+// 编译期初始化的全局变量，const创建常量，static创建静态变量，Atomic创建原子类型
+// 运行期初始化的全局变量，lazy_static用于懒初始化，Box::leak利用内存泄漏将一个变量的生命周期变为'static
+// 使用标准库cell::OnceCell 和 sync::OnceLock 前者用于单线程，后者用于多线程，它们用来存储堆上的信息，并且具有最 多只能赋值一次的特性
