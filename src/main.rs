623a89b5887100199130ade1176a1441e03e5f63
付海静 <fuhaijing@gotion.com.cn>
2024-09-18 09:04:45 +0000

    使用特征对象解决不同类型异常返回的问题

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,26 @@
-use std::fs::File;
-use std::io::{self, Read};
-use std::num;
+use std::fs::read_to_string;
 
-#[derive(Debug)]
-struct AppError {
-    kind: String,
-    message: String,
-}
+// fn main() -> Result<(), std::io::Error> {
+//     let html = render()?;
+//     println!("{}", html);
+//     Ok(())
+// }
 
-impl From<io::Error> for AppError {
-    fn from(error: io::Error) -> Self {
-        AppError {
-            kind: String::from("io"),
-            message: error.to_string(),
-        }
-    }
-}
+// fn render() -> Result<String, std::io::Error> {
+//     let file = std::env::var("MARKDOWN")?;
+//     let source = read_to_string(file)?;
+//     Ok(source)
+// }
 
-impl From<num::ParseIntError> for AppError {
-    fn from(error: num::ParseIntError) -> Self {
-        AppError {
-            kind: String::from("parse"),
-            message: error.to_string(),
-        }
-    }
-}
-
-fn main() -> Result<(), AppError> {
-    let mut file = File::open("hello_world.txt")?;
-
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
-
-    let _number: usize;
-    _number = content.parse()?;
-
+use std::error::Error;
+fn main() -> Result<(), Box<dyn Error>> {
+    let html = render()?;
+    println!("{}", html);
     Ok(())
 }
 
-// --------------- 上述代码运行后的可能输出 ---------------
-
-// 01. 若 hello_world.txt 文件不存在
-// Error: AppError { kind: "io", message: "No such file or directory (os error 2)" }
-
-// 02. 若用户没有相关的权限访问 hello_world.txt
-// Error: AppError { kind: "io", message: "Permission denied (os error 13)" }
-
-// 03. 若 hello_world.txt 包含有非数字的内容，例如 Hello, world!
-// Error: AppError { kind: "parse", message: "invalid digit found in string" }
+fn render() -> Result<String, Box<dyn Error>> {
+    let file = std::env::var("MARKDOWN")?;
+    let source = read_to_string(file)?;
+    Ok(source)
+}
