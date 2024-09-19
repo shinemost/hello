4ee184caacd8aa94a64cf75b28b82be2b037c1ef
付海静 <fuhaijing@gotion.com.cn>
2024-09-19 03:12:56 +0000

    使用anyhow解决自定义错误转换的问题，注意与thiserror的区别

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use std::fs::read_to_string;
 
-error_chain::error_chain! {
-  foreign_links {
-    EnvironmentVariableNotFound(::std::env::VarError);
-    IOError(::std::io::Error);
-  }
-}
+use anyhow::Result;
 
 fn main() -> Result<()> {
     let html = render()?;
@@ -18,3 +13,6 @@ fn render() -> Result<String> {
     let source = read_to_string(file)?;
     Ok(source)
 }
+
+// 如果你想要设计自己的错误类型，同时给调用者提供具体的信息时，就使用 thiserror，例如当你在开发一个三方库代码时。
+// 如果你只想要简单，就使用 anyhow，例如在自己的应用服务中。
