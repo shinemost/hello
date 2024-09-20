f61edcc8be4649b8243d534e3609c61c4f28cc40
付海静 <fuhaijing@gotion.com.cn>
2024-09-20 02:00:49 +0000

    构体中使用线程局部变量

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
 use std::cell::RefCell;
-use std::thread;
 
-thread_local!(static FOO: RefCell<u32> = RefCell::new(1));
+struct Foo;
+impl Foo {
+    thread_local! {
+        static FOO: RefCell<usize> = RefCell::new(0);
+    }
+}
 
 fn main() {
-    FOO.with(|f| {
-        assert_eq!(*f.borrow(), 1);
-        *f.borrow_mut() = 2;
-    });
-
-    // 每个线程开始时都会拿到线程局部变量的FOO的初始值
-    let t = thread::spawn(move || {
-        FOO.with(|f| {
-            assert_eq!(*f.borrow(), 1);
-            *f.borrow_mut() = 3;
-        });
-    });
-
-    // 等待线程完成
-    t.join().unwrap();
-
-    // 尽管子线程中修改为了3，我们在这里依然拥有main线程中的局部值：2
-    FOO.with(|f| {
-        assert_eq!(*f.borrow(), 2);
-    });
+    Foo::FOO.with(|x| println!("{:?}", x));
 }
