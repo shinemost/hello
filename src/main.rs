0339cb32c5d6594662acbbb8cc12e26021e2a3d4
shinemost <supertain147@163.com>
2024-09-22 21:39:30 +0800

    获取并发能力方法available_parallelism

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-use std::{thread, time::Duration};
-fn main() {
-    let handler = thread::Builder::new()
-        .name("named thread".into())
-        .spawn(|| {
-            println!("parking the thread");
-            thread::park();
-            println!("Thread unparked");
-        })
-        .unwrap();
-    thread::sleep(Duration::from_secs(2));
+use std::{io, thread};
 
-    println!("unpack the thread");
-    handler.thread().unpark();
-
-    handler.join().unwrap();
+fn main() -> io::Result<()> {
+    // 并发能力是一种资源，一个机器能够提供并发的能力值，这个数值一般等价于计算机拥 有的 CPU 数(逻辑的核数)，
+    // 但是在虚机和容器的环境下，程序可以使用的 CPU 核数 可能受到限制。
+    // 你可以通过 available_parallelism 获取当前的并发数:
+    let count = thread::available_parallelism()?.get();
+    println!("{count}");
+    assert!(count >= 1_usize);
+    Ok(())
 }
