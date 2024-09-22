d989bf4f57c2c2e1a76e5c82fede022eb10f56f4
shinemost <supertain147@163.com>
2024-09-22 22:16:27 +0800

    获取相关CPU核数等

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::{io, thread};
+use num_cpus;
+use num_threads;
+use std::thread;
 
-fn main() -> io::Result<()> {
-    // 并发能力是一种资源，一个机器能够提供并发的能力值，这个数值一般等价于计算机拥 有的 CPU 数(逻辑的核数)，
-    // 但是在虚机和容器的环境下，程序可以使用的 CPU 核数 可能受到限制。
-    // 你可以通过 available_parallelism 获取当前的并发数:
-    let count = thread::available_parallelism()?.get();
-    println!("{count}");
-    assert!(count >= 1_usize);
-    Ok(())
+fn main() {
+    // 获取CPU逻辑个数
+    let num = num_cpus::get();
+    println!("cores : {num}");
+
+    let count = thread::available_parallelism().unwrap().get();
+    println!("available_parallelism: {}", count);
+
+    if let Some(count) = num_threads::num_threads() {
+        println!("num_threads: {}", count);
+    } else {
+        // num_threads不支持windows平台
+        println!("num_threads: not supported");
+        let count = thread_amount::thread_amount();
+        if !count.is_none() {
+            println!("thread_amount: {}", count.unwrap());
+        }
+
+        let count = num_cpus::get();
+        println!("num_cpus: {}", count);
+    }
 }
