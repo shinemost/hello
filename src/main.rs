17c8b2e82d56d7918f116f292016a09e268d6b87
shinemost <supertain147@163.com>
2024-09-22 23:56:28 +0800

    park和unpark不能重复执行

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ pub fn thread_park2() {
     let handle = thread::spawn(|| {
         thread::sleep(Duration::from_millis(1000));
         thread::park();
+        // 后面的park会阻塞并且不能被释放
+        thread::park();
+        thread::park();
         println!("Hello from a park thread in case of unpark first!");
     });
-    // 先执行unpark会导致park后立即返回
+    // 如果预先调用一股脑的 unpark 多次，然后再一股脑的调用 park 行不行
+    // 答案是不行。因为一个线程只有一个令牌，这个令牌或者存在或者只有一个，
+    // 多次调用 unpark 也是针对一个令牌进行的的操作，上面的代码会导致新建的那个线程一直处于 parked 状态。
+    handle.thread().unpark();
+    handle.thread().unpark();
     handle.thread().unpark();
     handle.join().unwrap();
 }
-
 fn main() {
     thread_park2();
 }
