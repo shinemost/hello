63596ababab6f5580d72ec6cf23b9de138e7e00a
shinemost <supertain147@163.com>
2024-09-23 22:29:22 +0800

    thread_local! storage类似java的ThreadLocal

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,35 @@
-// pub fn wrong_start_threads_without_scoped() {
-//     let mut a = vec![1, 2, 3];
-//     let mut x = 0;
-//     thread::spawn(move || {
-//         println!("hello from the first scoped thread");
-//         // a的所有权已经被转移了
-//         dbg!(&a);
-//     });
-//     thread::spawn(move || {
-//         println!("hello from the second scoped thread");
-//         x += a[0] + a[2];
-//     });
-//     println!("hello from the main thread");
-//     // After the scope, we can modify and access our variables again:
-//     a.push(4);
-//     assert_eq!(x, a.len());
-// }
-
+use std::cell::RefCell;
 use std::thread;
 
-pub fn start_scoped_threads() {
-    let mut a = vec![1, 2, 3];
-    let mut x = 0;
-    // 它可以借用 scope 外部的非 ‘static’ 数据。使用 thread::scope 函数提供的 Scope 的参数，可以创建 (spawn) scoped thread。
-    // 创 建出来的 scoped thread 如果没有手工调用 join , 在这个函数返回前会自动 join
-    thread::scope(|s| {
-        s.spawn(|| {
-            println!("hello from the first scoped thread");
-            dbg!(&a);
-        });
-        s.spawn(|| {
-            println!("hello from the second scoped thread");
-            x += a[0] + a[2];
-        });
-        println!("hello from the main thread");
+pub fn start_threads_with_thread_local() {
+    // 定义线程全局变量
+    thread_local!(static COUNTER: RefCell<u32> = RefCell::new(1));
+    COUNTER.with(|c| {
+        *c.borrow_mut() = 2;
     });
-    // After the scope, we can modify and access our variables again:
-    a.push(4);
-    assert_eq!(x, a.len());
+    let handle1 = thread::spawn(move || {
+        // 此处都是全局变量的副本，只针对当前拥有的线程
+        COUNTER.with(|c| {
+            *c.borrow_mut() = 3;
+        });
+        COUNTER.with(|c| {
+            println!("Hello from a thread7, c={}!", *c.borrow());
+        });
+    });
+    let handle2 = thread::spawn(move || {
+        COUNTER.with(|c| {
+            *c.borrow_mut() = 4;
+        });
+        COUNTER.with(|c| {
+            println!("Hello from a thread8, c={}!", *c.borrow());
+        });
+    });
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+    COUNTER.with(|c| {
+        println!("Hello from main, c={}!", *c.borrow());
+    });
 }
 fn main() {
-    start_scoped_threads();
+    start_threads_with_thread_local();
 }
