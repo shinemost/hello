d0d764490dca2dde39ae78fabac4b7b0ddf6c844
shinemost <supertain147@163.com>
2024-09-23 22:58:45 +0800

    使用三方库thread-control来控制子线程

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,23 @@
-use std::cell::RefCell;
 use std::thread;
+use std::time::Duration;
+use thread_control::make_pair;
 
-pub fn start_threads_with_thread_local() {
-    // 定义线程全局变量
-    thread_local!(static COUNTER: RefCell<u32> = RefCell::new(1));
-    COUNTER.with(|c| {
-        *c.borrow_mut() = 2;
+pub fn control_thread() {
+    let (flag, control) = make_pair();
+    let handle = thread::spawn(move || {
+        while flag.alive() {
+            thread::sleep(Duration::from_millis(100));
+            println!("I'm alive!");
+        }
     });
-    let handle1 = thread::spawn(move || {
-        // 此处都是全局变量的副本，只针对当前拥有的线程
-        COUNTER.with(|c| {
-            *c.borrow_mut() = 3;
-        });
-        COUNTER.with(|c| {
-            println!("Hello from a thread7, c={}!", *c.borrow());
-        });
-    });
-    let handle2 = thread::spawn(move || {
-        COUNTER.with(|c| {
-            *c.borrow_mut() = 4;
-        });
-        COUNTER.with(|c| {
-            println!("Hello from a thread8, c={}!", *c.borrow());
-        });
-    });
-    handle1.join().unwrap();
-    handle2.join().unwrap();
-    COUNTER.with(|c| {
-        println!("Hello from main, c={}!", *c.borrow());
-    });
+    thread::sleep(Duration::from_millis(100));
+    assert_eq!(control.is_done(), false);
+    control.stop(); // Also you can `control.interrupt()` it
+    handle.join().unwrap();
+    assert_eq!(control.is_interrupted(), false);
+    assert_eq!(control.is_done(), true);
+    println!("This thread is stopped")
 }
 fn main() {
-    start_threads_with_thread_local();
+    control_thread();
 }
