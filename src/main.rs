26dc156456829414d29221123c9a6bf0cbfd39ec
shinemost <supertain147@163.com>
2024-10-05 16:03:39 +0800

    使用三方库rayon作为线程池构建scope thread

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 fn main() {
-    rayon_thread_pool();
+    rayon_thread_pool2();
 }
 
-fn fibonacci(n: u32) -> u32 {
-    if n == 0 || n == 1 {
-        return n;
-    }
-    let (a, b) = rayon::join(|| fibonacci(n - 1), || fibonacci(n - 2));
-    a + b
-}
-
-pub fn rayon_thread_pool() {
-    let pool = rayon::ThreadPoolBuilder::new().num_threads(8).build().unwrap();
-    let n = pool.install(|| fibonacci(2));
-    println!("{}", n);
+scoped_tls::scoped_thread_local!(static POOL_DATA:Vec<i32>);
+pub fn rayon_thread_pool2() {
+    let pool_data = vec![1, 2, 3];
+    // We haven’t assigned any TLS data yet.
+    assert!(!POOL_DATA.is_set());
+    rayon::ThreadPoolBuilder::new().build_scoped(
+        // 第一个闭包用于定义每个线程在启动时要执行的操作。它将 pool_data 的引用设置为 POOL_DATA 的线程本地存储值，
+        // 并在一个新的线程中运行 thread.run()，这 个闭包的目的是为每个线程设置线程本地存储数据
+        |thread| POOL_DATA.set(&pool_data, || thread.run()),
+        // 第二个闭包定义了线程池启动后要执行的操作。它使用 pool.install 方法来确保在线 程池中的每个线程中都能够访问到线程本地存储的值，
+        // 并且执行了一个断言 来验证 POOL_DATA 在这个线程的线程本地存储中已经被设置。
+        |pool| pool.install(|| assert!(POOL_DATA.is_set())),
+    ).unwrap();
+    // 在线程池的作用域结束后，这一行代码用来释放 pool_data 变量。这是因为线程本地存储中的值是按线程管理的，
+    // 所以在这个作用域结束后， 我们需要手动释放 pool_data，以确保它不再被任何线程访问。
+    drop(pool_data);
 }
 
